f8bb4f9b7781dbe1b046416e263068c08cb2a398
kallaballa <amir@viel-zu.org>
2019-08-08 19:23:52 +0200

    acclerometers now control HSL

--- src/sierpinski.cpp
+++ src/sierpinski.cpp
@@ -22,42 +22,41 @@
 #include "canvas.hpp"
 #include "button.hpp"
 #include "average.hpp"
-#include "trans.hpp"
 #include "color.hpp"
 
-constexpr size_t WIDTH = 100;
-constexpr size_t HEIGHT = 10;
+constexpr off_t WIDTH = 100;
+constexpr off_t HEIGHT = 10;
 
-std::mutex vpMutex;
+std::mutex VP_MTX;
 struct ViewPort {
 	off64_t x_ = 0;
 	off64_t y_ = 0;
 	double speedX_ = 0;
 	double speedY_ = 0;
+	double xa_ = 0;
+	double ya_ = 0;
+	double za_ = 0;
+	double hue_ = 0;
+	double saturation_ = 0;
+	double lightness_ = 0;
 };
 
 ViewPort VIEWPORT;
 
-AverageReadings avgRoll;
-AverageReadings avgPitch;
-AverageReadings avgAccX;
-AverageReadings avgAccY;
-AverageReadings avgAccZ;
+RtMidiIn *MIDI_IN = new RtMidiIn();
+std::vector<uint8_t> MESSAGE;
+bool DONE;
+static void FINISH(int ignore){ DONE = true; }
 
-RtMidiIn *midiin = new RtMidiIn();
-std::vector<uint8_t> message;
-bool done;
-static void finish(int ignore){ done = true; }
-
-std::mutex eventMutex;
+std::mutex EVENT_MUTEX;
 
 struct WMEvent {
 	int ctrlNr_ = 0;
   double roll_ = 0;
   double pitch_ = 0;
-	double x_a_ = 0;
-	double y_a_ = 0;
-	double z_a_ = 0;
+	double xa_ = 0;
+	double ya_ = 0;
+	double za_ = 0;
   Button btn_up_;
   Button btn_down_;
   Button btn_left_;
@@ -71,13 +70,17 @@ struct WMEvent {
   Button btn_2_;
 };
 
-WMEvent event;
+WMEvent EVENT;
 HSLColor hsl = { 0, 50, 50};
+
+std::mutex TEXTURE_MTX;
+std::vector<cv::Mat> TEXTURES;
+off_t TEXTURE_IDX = 0;
 
 void blend(const cv::Mat& src1, const cv::Mat& src2, const double alpha, const cv::Mat& dst)
 {
 	using namespace cv;
-   double beta; double input;
+   double beta;
 
    beta = ( 1.0 - alpha );
    addWeighted( src1, alpha, src2, beta, 0.0, dst);
@@ -86,7 +89,7 @@ void blend(const cv::Mat& src1, const cv::Mat& src2, const double alpha, const c
 void extractROI(const cv::Mat& texture, cv::Mat& view) {
 	//FIXME ############### off_t instead of size_t for coordinates; ################
 
-	vpMutex.lock();
+	VP_MTX.lock();
 
 	if(VIEWPORT.x_ >= texture.cols)
 		VIEWPORT.x_ %= texture.cols;
@@ -100,8 +103,11 @@ void extractROI(const cv::Mat& texture, cv::Mat& view) {
 
 	off64_t vx = VIEWPORT.x_;
 	off64_t vy = VIEWPORT.y_;
+	double h = VIEWPORT.hue_;
+	double s = VIEWPORT.lightness_;
+	double l = VIEWPORT.saturation_;
 
-	vpMutex.unlock();
+	VP_MTX.unlock();
 
 	for(off64_t c = 0; c < WIDTH; ++c) {
 		for(off64_t r = 0; r < HEIGHT; ++r) {
@@ -144,15 +150,29 @@ void extractROI(const cv::Mat& texture, cv::Mat& view) {
 			}
 		}
 	}
+	cv::Mat viewRGB(view.cols, view.rows, CV_8UC4);
+
+	for(size_t c = 0; c < view.cols; ++c) {
+		for(size_t r = 0; r < view.rows; ++r) {
+			uint32_t& p = view.at<uint32_t>(r, c);
+			RGBColor before(p);
+			HSLColor hsl(before);
+			hsl.adjustHue(h);
+			hsl.adjustSaturation(s);
+			hsl.adjustLightness(l);
+			RGBColor after(hsl);
+			p = after.val();
+		}
+	}
 }
 
 void colorAt(cv::Mat& m, int x, int y )
 {
-	eventMutex.lock();
-	hsl.h_ = x * 2 + event.x_a_;
-	hsl.s_ = (150 - event.y_a_);
+	EVENT_MUTEX.lock();
+	hsl.h_ = x * 2 + EVENT.xa_;
+	hsl.s_ = (150 - EVENT.ya_);
 //	hsl.adjustLightness((50 - z_a) / 10);
-	eventMutex.unlock();
+	EVENT_MUTEX.unlock();
 
 	RGBColor rgb(hsl);
   m.at<int32_t>(y,x) = rgb.val();
@@ -211,9 +231,9 @@ void draw(Canvas* canvas, cv::Mat& m) {
       if(j >= surface->h)
         continue;
 //      std::cerr << std::hex << m.at<int32_t>(i,j) << std::dec << std::endl;
-      for (off_t k = 0; k < 5; k++) {
-        for (off_t l = 0; l < 5; l++) {
-        	putpixel(surface, j * 5 + l, i * 5 + k, m.at<int32_t>(i,j));
+      for (off_t k = 0; k < 20; k++) {
+        for (off_t l = 0; l < 20; l++) {
+        	putpixel(surface, j * 20 + l, i * 20 + k, m.at<int32_t>(i,j));
         }
       }
     }
@@ -223,57 +243,71 @@ void draw(Canvas* canvas, cv::Mat& m) {
 }
 
 int main(int argc, char** argv) {
-	if(argc != 2) {
-		std::cerr << "Usage: slide <png-file>" << std::endl;
+	if(argc < 2) {
+		std::cerr << "Usage: slide <png-file-1> <png-file-2> ..." << std::endl;
 		exit(1);
 	}
-	cv::Mat textureRGB = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-	cv::Mat texture;
-	cv::cvtColor(textureRGB, texture, CV_RGB2RGBA);
-//	texture.convertTo(texture, CV_32S);
 
-  midiin->openPort( 0 );
-  midiin->ignoreTypes( false, false, false );
-  done = false;
-  signal(SIGINT, finish);
+	for(off_t i = 1; i < argc; ++i) {
+		cv::Mat textureRGB = cv::imread(argv[i], CV_LOAD_IMAGE_COLOR);
+		cv::Mat texture;
+		cv::cvtColor(textureRGB, texture, CV_RGB2RGBA);
+		TEXTURES.push_back(texture);
+	}
 
-  Canvas* canvas = new Canvas(WIDTH * 5, HEIGHT * 10,false);
+  MIDI_IN->openPort( 0 );
+  MIDI_IN->ignoreTypes( false, false, false );
+  DONE = false;
+  signal(SIGINT, FINISH);
+
+  Canvas* canvas = new Canvas(WIDTH * 20, HEIGHT * 40,false);
 
   std::thread midiThread([&]() {
     int nBytes;
 
-    while ( !done ) {
-      midiin->getMessage( &message );
+    while ( !DONE ) {
+      MIDI_IN->getMessage( &MESSAGE );
 
-      nBytes = message.size();
+      nBytes = MESSAGE.size();
 
       if(nBytes == 10) {
-      	eventMutex.lock();
-      	event.ctrlNr_ = message[1];
+      	EVENT_MUTEX.lock();
+      	EVENT.ctrlNr_ = MESSAGE[1];
 //      	event.roll_ = avgRoll.smooth(message[2]);
 //      	event.pitch_ = avgPitch.smooth(message[3]);
 //      	event.x_a_ = avgAccX.smooth(message[4]);
 //      	event.y_a_ = avgAccY.smooth(message[5]);
 //      	event.z_a_ = avgAccZ.smooth(message[6]);
-      	event.roll_ = message[2];
-      	event.pitch_ = message[3];
-      	event.x_a_ = message[4];
-      	event.y_a_ = message[5];
-      	event.z_a_ = message[6];
-        uint16_t buttons = (((uint16_t) message[7])) | ((uint16_t) message[8] << 8);
+      	EVENT.roll_ = MESSAGE[2];
+      	EVENT.pitch_ = MESSAGE[3];
+      	EVENT.xa_ = MESSAGE[4];
+      	EVENT.ya_ = MESSAGE[5];
+      	EVENT.za_ = MESSAGE[6];
+        uint16_t buttons = (((uint16_t) MESSAGE[7])) | ((uint16_t) MESSAGE[8] << 8);
 
-        event.btn_up_.update(buttons & MASK_BTN_UP);
-        event.btn_down_.update(buttons & MASK_BTN_DOWN);
-        event.btn_left_.update(buttons & MASK_BTN_LEFT);
-        event.btn_right_.update(buttons & MASK_BTN_RIGHT);
-        event.btn_a_.update(buttons & MASK_BTN_A);
-        event.btn_b_.update(buttons & MASK_BTN_B);
-        event.btn_minus_.update(buttons & MASK_BTN_MINUS);
-        event.btn_plus_.update(buttons & MASK_BTN_PLUS);
-        event.btn_home_.update(buttons & MASK_BTN_HOME);
-        event.btn_1_.update(buttons & MASK_BTN_1);
-        event.btn_2_.update(buttons & MASK_BTN_2);
-      	eventMutex.unlock();
+        EVENT.btn_up_.update(buttons & MASK_BTN_UP);
+        EVENT.btn_down_.update(buttons & MASK_BTN_DOWN);
+        EVENT.btn_left_.update(buttons & MASK_BTN_LEFT);
+        EVENT.btn_right_.update(buttons & MASK_BTN_RIGHT);
+        EVENT.btn_a_.update(buttons & MASK_BTN_A);
+        EVENT.btn_b_.update(buttons & MASK_BTN_B);
+        EVENT.btn_minus_.update(buttons & MASK_BTN_MINUS);
+        EVENT.btn_plus_.update(buttons & MASK_BTN_PLUS);
+        EVENT.btn_home_.update(buttons & MASK_BTN_HOME);
+        EVENT.btn_1_.update(buttons & MASK_BTN_1);
+        EVENT.btn_2_.update(buttons & MASK_BTN_2);
+
+        if(EVENT.btn_right_.release_)
+        	++TEXTURE_IDX;
+        else if(EVENT.btn_left_.release_)
+        	--TEXTURE_IDX;
+
+        if(TEXTURE_IDX >= TEXTURES.size())
+        	TEXTURE_IDX = 0;
+        else if(TEXTURE_IDX < 0)
+        	TEXTURE_IDX = TEXTURES.size() - 1;
+
+      	EVENT_MUTEX.unlock();
       }
 
       std::this_thread::yield();
@@ -282,18 +316,25 @@ int main(int argc, char** argv) {
   });
 
 	std::thread slideThread([&]() {
-		while(true) {
-			eventMutex.lock();
-			off_t xa = (event.roll_ - 64);
-			off_t ya = (event.pitch_ - 64) * -1;
-			std::cerr << "ya:" <<  ya << std::endl;
+		while(!DONE) {
+			EVENT_MUTEX.lock();
+			off_t roll = (EVENT.roll_ - 64);
+			off_t pitch = (EVENT.pitch_ - 64) * -1;
+			double xa = EVENT.xa_ - 50;
+			double ya = EVENT.ya_ - 50;
+			double za = EVENT.za_ - 50;
+			if(xa < 20)
+				xa = 0;
+			if(ya < 20)
+				ya = 0;
+			if(za < 20)
+				za = 0;
+			EVENT_MUTEX.unlock();
+			VP_MTX.lock();
 
-			eventMutex.unlock();
-			vpMutex.lock();
+				VIEWPORT.speedX_ += roll;
 
-				VIEWPORT.speedX_ += xa;
-
-				VIEWPORT.speedY_ += ya;
+				VIEWPORT.speedY_ += pitch;
 
 			if(std::abs(VIEWPORT.speedX_) / 20 < 0.01)
 				VIEWPORT.speedX_ = 0;
@@ -303,19 +344,30 @@ int main(int argc, char** argv) {
 			VIEWPORT.x_ += ceil(VIEWPORT.speedX_ / 20);
 			VIEWPORT.y_ += ceil(VIEWPORT.speedY_ / 50);
 
-			if(VIEWPORT.x_ >= texture.cols) {
-				VIEWPORT.x_ = VIEWPORT.x_ % texture.cols;
+			VIEWPORT.hue_ += xa;
+			VIEWPORT.lightness_ += ya;
+			VIEWPORT.saturation_ += za;
+
+			TEXTURE_MTX.lock();
+			if(VIEWPORT.x_ >= TEXTURES[TEXTURE_IDX].cols) {
+				VIEWPORT.x_ = VIEWPORT.x_ % TEXTURES[TEXTURE_IDX].cols;
 			} else if(VIEWPORT.x_ < 0) {
-				VIEWPORT.x_ = texture.cols + VIEWPORT.x_;
+				VIEWPORT.x_ = TEXTURES[TEXTURE_IDX].cols + VIEWPORT.x_;
 			}
-			if(VIEWPORT.y_ >= texture.rows) {
-				VIEWPORT.y_ = VIEWPORT.y_ % texture.rows;
+			if(VIEWPORT.y_ >= TEXTURES[TEXTURE_IDX].rows) {
+				VIEWPORT.y_ = VIEWPORT.y_ % TEXTURES[TEXTURE_IDX].rows;
 			} else if(VIEWPORT.y_ < 0) {
-				VIEWPORT.y_ = texture.rows + VIEWPORT.y_;
+				VIEWPORT.y_ = TEXTURES[TEXTURE_IDX].rows + VIEWPORT.y_;
 			}
+			TEXTURE_MTX.unlock();
+
 			VIEWPORT.speedX_ /= 2;
 			VIEWPORT.speedY_ /= 2;
-			vpMutex.unlock();
+			VIEWPORT.hue_ /= 2;
+			VIEWPORT.lightness_ /= 2;
+			VIEWPORT.saturation_ /= 2;
+
+			VP_MTX.unlock();
 			std::this_thread::yield();
 			usleep(40000);
 		}
@@ -323,26 +375,29 @@ int main(int argc, char** argv) {
 
   SDL_Event event;
   cv::Mat* view = new cv::Mat(HEIGHT, WIDTH, CV_8UC4);
-  cv::Mat* last = new cv::Mat(HEIGHT, WIDTH, CV_8UC4);
-  cv::Mat *dst = new cv::Mat(HEIGHT, WIDTH, CV_8UC4);
-  while(!done) {
+//  cv::Mat* last = new cv::Mat(HEIGHT, WIDTH, CV_8UC4);
+//  cv::Mat *dst = new cv::Mat(HEIGHT, WIDTH, CV_8UC4);
+  while(!DONE) {
     if(SDL_PollEvent(&event)) {
       if(event.type == SDL_QUIT) {
         exit(0);
       }
     }
-    extractROI(texture, *view);
-    if(!last->empty()) {
-    	for(size_t i = 1; i < 4; ++i) {
-    		blend(*last, *view, 1.0/i, *dst);
-        draw(canvas, *dst);
-        usleep(10000);
-    	}
-    }
+
+    TEXTURE_MTX.lock();
+    extractROI(TEXTURES[TEXTURE_IDX], *view);
+    TEXTURE_MTX.unlock();
+//    if(!last->empty()) {
+//    	for(size_t i = 1; i < 4; ++i) {
+//    		blend(*last, *view, 1.0/4 - i, *dst);
+//        draw(canvas, *dst);
+//        usleep(10000);
+//    	}
+//    }
     draw(canvas, *view);
-    view->copyTo(*last);
+//    view->copyTo(*last);
     std::this_thread::yield();
-    usleep(10000);
+    usleep(40000);
   }
 
   return 0;
